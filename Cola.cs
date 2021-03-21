0d5686dc1d91d17e0dc24ede4fb753af01f790a5
Araniva27 <manuelaraniva07@gmail.com>
2021-03-21 15:28:57 -0600

    Creacion de clase Pedido, Nodo y Cola

---
+++ Cola.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proyecto_Catedra_PEDG01T
+{
+    class Cola
+    {
+        private Nodo primero { get; set; }
+        private Nodo ultimo { get; set; }
+
+        public Cola()
+        {
+            primero = ultimo = null;
+        }
+
+        public void Encolar(int idPedido, string fechaPedido, int estadoPedido)
+        {
+            Nodo entrada = new Nodo();
+            Pedido pedido = new Pedido();
+           
+            pedido.IdPedido = idPedido;
+            pedido.FechaPedido = fechaPedido;
+            pedido.EstadoPedido = estadoPedido;
+           
+            entrada.Info = pedido;
+            
+            if(primero == null)
+            {
+                primero = ultimo = entrada;
+                entrada.Siquiente = null;
+            }
+            else
+            {
+                ultimo.Siquiente = entrada;
+                entrada.Siquiente = null;
+                ultimo = entrada;
+            }
+        }
+
+        public void Desencolar()
+        {
+            if(primero == null)
+            {
+                MessageBox.Show("La cola se encuentra vacía", "INFORMACIÓN", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                primero = primero.Siquiente;
+            }
+        }
+    }
+}
---
+++ Pedido.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Catedra_PEDG01T
+{
+    class Pedido
+    {
+        private int idPedido;
+        private string fechaPedido;
+        private int estadoPedido;
+
+        public int IdPedido
+        {
+            get { return idPedido; }
+            set { idPedido = value; }
+        }
+
+        public string FechaPedido
+        {
+            get { return fechaPedido; }
+            set { fechaPedido = value; }
+        }
+
+        public int EstadoPedido
+        {
+            get { return estadoPedido; }
+            set { estadoPedido = value; }
+        }
+
+    }
+}
