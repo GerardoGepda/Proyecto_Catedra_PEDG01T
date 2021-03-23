507454b5149feafed621031f55a2f3b3a635e9d6
Fernando Bonilla <fernandobonilla875@gmail.com>
2021-03-23 10:46:15 -0600

    Diseño de PedidosForm y PerfilForm

--- PedidosForm.Designer.cs
+++ PedidosForm.Designer.cs
@@ -28,7 +28,99 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnentregar = new System.Windows.Forms.Button();
+            this.lblpedidos = new System.Windows.Forms.Label();
+            this.lblencola = new System.Windows.Forms.Label();
+            this.btnfactura = new System.Windows.Forms.Button();
+            this.btneditar = new System.Windows.Forms.Button();
+            this.btncancelar = new System.Windows.Forms.Button();
+            this.dgvpedidos = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvpedidos)).BeginInit();
             this.SuspendLayout();
+            // 
+            // btnentregar
+            // 
+            this.btnentregar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnentregar.FlatAppearance.BorderSize = 0;
+            this.btnentregar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnentregar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnentregar.ForeColor = System.Drawing.Color.White;
+            this.btnentregar.Location = new System.Drawing.Point(333, 77);
+            this.btnentregar.Name = "btnentregar";
+            this.btnentregar.Size = new System.Drawing.Size(107, 33);
+            this.btnentregar.TabIndex = 0;
+            this.btnentregar.Text = "Entregar pedido";
+            this.btnentregar.UseVisualStyleBackColor = false;
+            // 
+            // lblpedidos
+            // 
+            this.lblpedidos.AutoSize = true;
+            this.lblpedidos.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblpedidos.Location = new System.Drawing.Point(34, 27);
+            this.lblpedidos.Name = "lblpedidos";
+            this.lblpedidos.Size = new System.Drawing.Size(83, 25);
+            this.lblpedidos.TabIndex = 4;
+            this.lblpedidos.Text = "Pedidos";
+            // 
+            // lblencola
+            // 
+            this.lblencola.AutoSize = true;
+            this.lblencola.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblencola.Location = new System.Drawing.Point(54, 86);
+            this.lblencola.Name = "lblencola";
+            this.lblencola.Size = new System.Drawing.Size(105, 16);
+            this.lblencola.TabIndex = 5;
+            this.lblencola.Text = "Pedido en cola: ";
+            // 
+            // btnfactura
+            // 
+            this.btnfactura.BackColor = System.Drawing.Color.Brown;
+            this.btnfactura.FlatAppearance.BorderSize = 0;
+            this.btnfactura.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnfactura.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnfactura.ForeColor = System.Drawing.Color.White;
+            this.btnfactura.Location = new System.Drawing.Point(460, 77);
+            this.btnfactura.Name = "btnfactura";
+            this.btnfactura.Size = new System.Drawing.Size(107, 33);
+            this.btnfactura.TabIndex = 6;
+            this.btnfactura.Text = "Mostrar factura";
+            this.btnfactura.UseVisualStyleBackColor = false;
+            // 
+            // btneditar
+            // 
+            this.btneditar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btneditar.FlatAppearance.BorderSize = 0;
+            this.btneditar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btneditar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btneditar.ForeColor = System.Drawing.Color.White;
+            this.btneditar.Location = new System.Drawing.Point(589, 77);
+            this.btneditar.Name = "btneditar";
+            this.btneditar.Size = new System.Drawing.Size(107, 33);
+            this.btneditar.TabIndex = 7;
+            this.btneditar.Text = "Editar";
+            this.btneditar.UseVisualStyleBackColor = false;
+            // 
+            // btncancelar
+            // 
+            this.btncancelar.BackColor = System.Drawing.Color.Red;
+            this.btncancelar.FlatAppearance.BorderSize = 0;
+            this.btncancelar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btncancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btncancelar.ForeColor = System.Drawing.Color.White;
+            this.btncancelar.Location = new System.Drawing.Point(716, 77);
+            this.btncancelar.Name = "btncancelar";
+            this.btncancelar.Size = new System.Drawing.Size(107, 33);
+            this.btncancelar.TabIndex = 8;
+            this.btncancelar.Text = "Cancelar";
+            this.btncancelar.UseVisualStyleBackColor = false;
+            // 
+            // dgvpedidos
+            // 
+            this.dgvpedidos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvpedidos.Location = new System.Drawing.Point(57, 172);
+            this.dgvpedidos.Name = "dgvpedidos";
+            this.dgvpedidos.Size = new System.Drawing.Size(766, 340);
+            this.dgvpedidos.TabIndex = 9;
             // 
             // PedidosForm
             // 
@@ -36,13 +128,30 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(900, 580);
+            this.Controls.Add(this.dgvpedidos);
+            this.Controls.Add(this.btncancelar);
+            this.Controls.Add(this.btneditar);
+            this.Controls.Add(this.btnfactura);
+            this.Controls.Add(this.lblencola);
+            this.Controls.Add(this.lblpedidos);
+            this.Controls.Add(this.btnentregar);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "PedidosForm";
             this.Text = "PedidosForm";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvpedidos)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnentregar;
+        private System.Windows.Forms.Label lblpedidos;
+        private System.Windows.Forms.Label lblencola;
+        private System.Windows.Forms.Button btnfactura;
+        private System.Windows.Forms.Button btneditar;
+        private System.Windows.Forms.Button btncancelar;
+        private System.Windows.Forms.DataGridView dgvpedidos;
     }
 }
--- PerfilForm.Designer.cs
+++ PerfilForm.Designer.cs
@@ -28,7 +28,120 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblperfil = new System.Windows.Forms.Label();
+            this.pbxperfil = new System.Windows.Forms.PictureBox();
+            this.lblnombre = new System.Windows.Forms.Label();
+            this.txtnombre = new System.Windows.Forms.TextBox();
+            this.lbluser = new System.Windows.Forms.Label();
+            this.txtuser = new System.Windows.Forms.TextBox();
+            this.lblemail = new System.Windows.Forms.Label();
+            this.txtemail = new System.Windows.Forms.TextBox();
+            this.lbltelefono = new System.Windows.Forms.Label();
+            this.txttelefono = new System.Windows.Forms.TextBox();
+            this.btneditar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxperfil)).BeginInit();
             this.SuspendLayout();
+            // 
+            // lblperfil
+            // 
+            this.lblperfil.AutoSize = true;
+            this.lblperfil.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblperfil.Location = new System.Drawing.Point(38, 26);
+            this.lblperfil.Name = "lblperfil";
+            this.lblperfil.Size = new System.Drawing.Size(55, 25);
+            this.lblperfil.TabIndex = 1;
+            this.lblperfil.Text = "Perfil";
+            // 
+            // pbxperfil
+            // 
+            this.pbxperfil.BackgroundImage = global::Proyecto_Catedra_PEDG01T.Properties.Resources.profile_person;
+            this.pbxperfil.Location = new System.Drawing.Point(377, 52);
+            this.pbxperfil.Name = "pbxperfil";
+            this.pbxperfil.Size = new System.Drawing.Size(146, 145);
+            this.pbxperfil.TabIndex = 0;
+            this.pbxperfil.TabStop = false;
+            // 
+            // lblnombre
+            // 
+            this.lblnombre.AutoSize = true;
+            this.lblnombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblnombre.Location = new System.Drawing.Point(431, 226);
+            this.lblnombre.Name = "lblnombre";
+            this.lblnombre.Size = new System.Drawing.Size(57, 16);
+            this.lblnombre.TabIndex = 2;
+            this.lblnombre.Text = "Nombre";
+            // 
+            // txtnombre
+            // 
+            this.txtnombre.Location = new System.Drawing.Point(332, 245);
+            this.txtnombre.Name = "txtnombre";
+            this.txtnombre.Size = new System.Drawing.Size(257, 20);
+            this.txtnombre.TabIndex = 3;
+            // 
+            // lbluser
+            // 
+            this.lbluser.AutoSize = true;
+            this.lbluser.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbluser.Location = new System.Drawing.Point(431, 302);
+            this.lbluser.Name = "lbluser";
+            this.lbluser.Size = new System.Drawing.Size(55, 16);
+            this.lbluser.TabIndex = 4;
+            this.lbluser.Text = "Usuario";
+            // 
+            // txtuser
+            // 
+            this.txtuser.Location = new System.Drawing.Point(332, 321);
+            this.txtuser.Name = "txtuser";
+            this.txtuser.Size = new System.Drawing.Size(257, 20);
+            this.txtuser.TabIndex = 5;
+            // 
+            // lblemail
+            // 
+            this.lblemail.AutoSize = true;
+            this.lblemail.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblemail.Location = new System.Drawing.Point(431, 380);
+            this.lblemail.Name = "lblemail";
+            this.lblemail.Size = new System.Drawing.Size(49, 16);
+            this.lblemail.TabIndex = 6;
+            this.lblemail.Text = "Correo";
+            // 
+            // txtemail
+            // 
+            this.txtemail.Location = new System.Drawing.Point(332, 399);
+            this.txtemail.Name = "txtemail";
+            this.txtemail.Size = new System.Drawing.Size(257, 20);
+            this.txtemail.TabIndex = 7;
+            // 
+            // lbltelefono
+            // 
+            this.lbltelefono.AutoSize = true;
+            this.lbltelefono.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbltelefono.Location = new System.Drawing.Point(426, 463);
+            this.lbltelefono.Name = "lbltelefono";
+            this.lbltelefono.Size = new System.Drawing.Size(62, 16);
+            this.lbltelefono.TabIndex = 8;
+            this.lbltelefono.Text = "Teléfono";
+            // 
+            // txttelefono
+            // 
+            this.txttelefono.Location = new System.Drawing.Point(332, 482);
+            this.txttelefono.Name = "txttelefono";
+            this.txttelefono.Size = new System.Drawing.Size(257, 20);
+            this.txttelefono.TabIndex = 9;
+            // 
+            // btneditar
+            // 
+            this.btneditar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btneditar.FlatAppearance.BorderSize = 0;
+            this.btneditar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btneditar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btneditar.ForeColor = System.Drawing.Color.White;
+            this.btneditar.Location = new System.Drawing.Point(744, 503);
+            this.btneditar.Name = "btneditar";
+            this.btneditar.Size = new System.Drawing.Size(107, 33);
+            this.btneditar.TabIndex = 10;
+            this.btneditar.Text = "Editar perfil";
+            this.btneditar.UseVisualStyleBackColor = false;
             // 
             // PerfilForm
             // 
@@ -36,13 +149,38 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(900, 580);
+            this.Controls.Add(this.btneditar);
+            this.Controls.Add(this.txttelefono);
+            this.Controls.Add(this.lbltelefono);
+            this.Controls.Add(this.txtemail);
+            this.Controls.Add(this.lblemail);
+            this.Controls.Add(this.txtuser);
+            this.Controls.Add(this.lbluser);
+            this.Controls.Add(this.txtnombre);
+            this.Controls.Add(this.lblnombre);
+            this.Controls.Add(this.lblperfil);
+            this.Controls.Add(this.pbxperfil);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "PerfilForm";
             this.Text = "PerfilForm";
+            ((System.ComponentModel.ISupportInitialize)(this.pbxperfil)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pbxperfil;
+        private System.Windows.Forms.Label lblperfil;
+        private System.Windows.Forms.Label lblnombre;
+        private System.Windows.Forms.TextBox txtnombre;
+        private System.Windows.Forms.Label lbluser;
+        private System.Windows.Forms.TextBox txtuser;
+        private System.Windows.Forms.Label lblemail;
+        private System.Windows.Forms.TextBox txtemail;
+        private System.Windows.Forms.Label lbltelefono;
+        private System.Windows.Forms.TextBox txttelefono;
+        private System.Windows.Forms.Button btneditar;
     }
 }
